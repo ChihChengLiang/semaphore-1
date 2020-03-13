b8841d46074aa2e00f0180a92aaeff98202a65eb
Chih Cheng Liang <chihchengliang@gmail.com>
2020-03-13 14:27:03 +0800

    full experiment

--- circuits/profile.js
+++ circuits/profile.js
@@ -4,46 +4,22 @@ const path = require('path')
 
 const zkSnark = require('snarkjs')
 const childProcess = require('child_process')
+const libsemaphore = require('libsemaphore')
 
-const runExperiment = async treeDepth => {
-  const experiment = 'exp1'
+const identity = libsemaphore.genIdentity()
+const idc = libsemaphore.genIdentityCommitment(identity)
+const externalNullifier = '0'
 
-  const experimentDir = path.join(__dirname, 'experiments', experiment)
-  fs.mkdirSync(experimentDir, { recursive: true })
-
-  const assignSignal = `
-  template A() {
-    signal output out;
-  
-    out = 3;  // This is an error that compile should detect
-  }
-  
-  component main = A();
-  `
-
-  const p = path.join(experimentDir, 'assignSginal.circom')
-  fs.writeFileSync(p, assignSignal)
-
-  const circuitPath = path.join(experimentDir, 'circuit.json')
-
-  if (!fs.existsSync(circuitPath)) {
-    console.log('circuit inexistent, compiling')
-    await compiler(p).then(cir =>
-      fs.writeFileSync(circuitPath, JSON.stringify(cir, null, 1), 'utf8')
-    )
-  }
-
-  const circuitDef = JSON.parse(fs.readFileSync(circuitPath, 'utf8'))
-  const circuit = new zkSnark.Circuit(circuitDef)
-
+const runTrustedSetup = async (
+  circuit,
+  provingKeyJSONPath,
+  verificationKeyJSONPath,
+  provingKeyBinPath
+) => {
+  console.log('run trusted setup')
+  console.time('setup')
   const setup = zkSnark.groth.setup(circuit)
-
-  const provingKeyJSONPath = path.join(experimentDir, './proving_key.json')
-  const verificationKeyJSONPath = path.join(
-    experimentDir,
-    './verification_key.json'
-  )
-  const provingKeyBinPath = path.join(experimentDir, './proving_key.bin')
+  console.timeEnd('setup')
   fs.writeFileSync(
     provingKeyJSONPath,
     JSON.stringify(zkSnark.stringifyBigInts(setup.vk_proof))
@@ -52,7 +28,6 @@ const runExperiment = async treeDepth => {
     verificationKeyJSONPath,
     JSON.stringify(zkSnark.stringifyBigInts(setup.vk_verifier))
   )
-
   const cp = childProcess.spawnSync('node', [
     'node_modules/websnark/tools/buildpkey.js',
     '-i',
@@ -62,29 +37,92 @@ const runExperiment = async treeDepth => {
   ])
   console.log(cp.stdout.toString())
   console.log(cp.stderr.toString())
+}
 
-  const input = {}
+const runExperiment = async treeDepth => {
+  const experiment = `treeDepth_${treeDepth}`
 
-  const provingStart = process.hrtime()
-  const witness = circuit.calculateWitness(input)
+  console.log('Running experiment', experiment)
 
-  const { proof, publicSignals } = zkSnark.groth.genProof(
-    setup.vk_proof,
-    witness
+  const experimentDir = path.join(__dirname, 'experiments', experiment)
+  console.log('experimentDir:', experimentDir)
+  fs.mkdirSync(experimentDir, { recursive: true })
+
+  const semaphoreCircom = `
+  include "./semaphore-base.circom";
+
+  component main = Semaphore(${treeDepth});
+  `
+
+  const p = path.join(__dirname, 'circom', 'semaphore.circom')
+  fs.writeFileSync(p, semaphoreCircom)
+
+  const circuitPath = path.join(experimentDir, 'circuit.json')
+
+  if (!fs.existsSync(circuitPath)) {
+    console.log('circuit inexistent, compiling')
+    console.time('CompilingCircuit')
+    await compiler(p).then(cir =>
+      fs.writeFileSync(circuitPath, JSON.stringify(cir, null, 1), 'utf8')
+    )
+    console.timeEnd('CompilingCircuit')
+  } else {
+    console.log('circuit exists, skip compiling')
+  }
+
+  const circuitDef = JSON.parse(fs.readFileSync(circuitPath, 'utf8'))
+  const circuit = new zkSnark.Circuit(circuitDef)
+
+  const provingKeyJSONPath = path.join(experimentDir, './proving_key.json')
+  const verificationKeyJSONPath = path.join(
+    experimentDir,
+    './verification_key.json'
   )
-  const provingEnd = process.hrtime(provingStart)
+  const provingKeyBinPath = path.join(experimentDir, './proving_key.bin')
 
-  const verificationStart = process.hrtime()
-  zkSnark.groth.isValid(setup.vk_verifier, proof, publicSignals)
-  const verificationEnd = process.hrtime(verificationStart)
+  if (!fs.existsSync(provingKeyJSONPath)) {
+    await runTrustedSetup(
+      circuit,
+      provingKeyJSONPath,
+      verificationKeyJSONPath,
+      provingKeyBinPath
+    )
+  } else {
+    console.log('Proving key exists, skip setup')
+  }
+
+  const provingKey = fs.readFileSync(provingKeyBinPath)
+  const verificationKey = libsemaphore.parseVerifyingKeyJson(
+    fs.readFileSync(verificationKeyJSONPath).toString()
+  )
+
+  const provingStart = process.hrtime.bigint()
+
+  const { witness } = await libsemaphore.genWitness(
+    'signal0',
+    circuit,
+    identity,
+    [idc],
+    treeDepth,
+    externalNullifier
+  )
+  console.log('Proving')
+  const proof = await libsemaphore.genProof(witness, provingKey)
+  const publicSignals = libsemaphore.genPublicSignals(witness, circuit)
+  const provingTime = process.hrtime.bigint() - provingStart
+
+  console.log('Verifying')
+  const verificationStart = process.hrtime.bigint()
+  zkSnark.groth.isValid(verificationKey, proof, publicSignals)
+  const verificationTime = process.hrtime.bigint() - verificationStart
 
   const report = {
     treeDepth,
     circuitSize: fs.statSync(circuitPath).size,
     provingKeySize: fs.statSync(provingKeyBinPath).size,
     verificationKeySize: fs.statSync(verificationKeyJSONPath).size,
-    provingTime: provingEnd[1],
-    verificationTime: verificationEnd[1]
+    provingTime: provingTime.toString(),
+    verificationTime: verificationTime.toString()
   }
   console.log(report)
   const reportPath = path.join(experimentDir, 'report.json')
@@ -92,6 +130,9 @@ const runExperiment = async treeDepth => {
 }
 
 const main = async () => {
-  await runExperiment(4)
+  const treeDepths = [4, 8, 12, 16, 20]
+  for (let td of treeDepths) {
+    await runExperiment(td)
+  }
 }
 main()
